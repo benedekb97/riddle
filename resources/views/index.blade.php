60596723ab3425d5c7e4129e52ec65dadcbc0bef
Benedek Burgess <benedekb97@gail.com>
2019-10-11 21:28:33 +0200

    Add info and some shite

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,18 +7,32 @@
                 <h4><i class="fa fa-info-circle"></i> Hasznos információk:</h4>
 
                 <ul>
-                    <li>Jelenleg bárki feltölthet riddle-t.</li>
-                    <li>Ezt el kell fogadnia egy moderátornak
+                    <li>Jelenleg "<strong>nyílt-ß tesztelés</strong>" fázisban van az oldal</li>
+                    <li><strong>Bárki</strong> tölthet fel riddle-t</li>
+                    <li>Ezt el kell fogadnia egy <strong>moderátornak</strong>
                         <ul>
                             <li>Ha elfogadja akkor bekerül a riddle listába</li>
                             <li>Ha nem akkor kapsz egy indokot hogy miért szar</li>
-                            <li>Miután kijavítottad megint el kell fogadnia/visszadobni</li>
+                            <li>Miután kijavítottad megint el kell fogadni/visszadobni</li>
                             <li>stb.</li>
                         </ul>
                     </li>
                     <li>Ha moderátor felületet akarod tesztelni írj nekem (Beni)</li>
-                    <li>Saját riddle-t is megoldhatsz tesztelés céljából</li>
-                    <li>LIVE-on az lesz, hogy csak a nem általad elfogadott riddle-öket oldhatod meg moderátorként</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-md-push-3">
+            <div class="alert alert-info">
+                <h4><i class="fa fa-info-circle"></i> Pontozás:</h4>
+
+                <ul>
+                    <li>Minden riddle-nek van egy <strong>nehézsége</strong> (1-5ig)</li>
+                    <li>Alapból ez <strong>5-el szorzódik</strong></li>
+                    <li>Az elhasznált hintek száma <strong>levonódik</strong> az 5-ből</li>
+                    <br>
+                    <li><strong>Röviden: pontok = nehézség*max(1,5-felhasznált hintek)</strong></li>
                 </ul>
             </div>
         </div>
--- resources/views/users/riddles.blade.php
+++ resources/views/users/riddles.blade.php
@@ -104,6 +104,20 @@
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <h4 class="modal-title">{{ $riddle->title }} - Információk</h4>
                     </div>
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Próba</th>
+                                <th>Hányszor próbálták</th>
+                            </tr>
+                            @foreach($riddle->guesses as $guess)
+                                <tr>
+                                    <td>{{ $guess->guess }}</td>
+                                    <td>{{ $guess->count }}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
                 </div>
 
             </div>
