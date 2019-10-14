046d8f921d7adc0580f3233760dfb092575fa7f5
Benedek Burgess <benedekb97@gail.com>
2019-10-14 17:57:12 +0200

    Add admin page and static messages

--- resources/views/riddles/blocked.blade.php
+++ resources/views/riddles/blocked.blade.php
@@ -19,6 +19,7 @@
                             <th>Kép</th>
                             <th>Készítő</th>
                             <th>Tiltás oka</th>
+                            <th>Hintek</th>
                             <th>Műveletek</th>
                         </tr>
                         @foreach($riddles as $riddle)
@@ -34,6 +35,14 @@
                                 </td>
                                 <td>{{ $riddle->user->name }}</td>
                                 <td>{{ $riddle->block_reason }}</td>
+
+                                <td>
+                                    <span data-toggle="tooltip" title="Hintek">
+                                        <button type="button" class="btn btn-xs btn-primary" data-toggle="modal" data-target="#hints_{{ $riddle->id }}">
+                                            <i class="fa fa-lighbulb"></i>
+                                        </button>
+                                    </span>
+                                </td>
                                 <td>
                                     <span data-toggle="tooltip" title="Engedélyezés">
                                         <a href="{{ route('riddles.approve', ['riddle' => $riddle, 'return' => 'blocked']) }}" class="btn btn-xs btn-success">
@@ -51,4 +60,33 @@
             </div>
         </div>
     </div>
+@endsection
+@section('modals')
+    @foreach($riddles as $riddle)
+    <div class="modal fade" id="hints_{{ $riddle->id }}">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Hintek</h4>
+                    <button class="close" data-dismiss="modal" type="button">&times;</button>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Hint</th>
+                        </tr>
+                        @foreach($riddle->hints as $hint)
+                            <tr>
+                                <td>{{ $hint->hint }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Mégse</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
 @endsection
