ab7be5f72e88a6be985c5d43692c83848ebb31c9
Klub PC <klub520@spetzialisch.tk>
2019-10-15 10:13:39 +0200

    Add 403 styles

--- resources/views/errors/403.php
+++ resources/views/errors/403.php
@@ -1,1 +1,24 @@
-get fcuked
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>403 - Hozzáférés megtagadva</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/492a2c0a6b.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-push-6">
+                <div class="alert alert-danger">
+                    <h4>
+                        <i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;Hozzáférés megtagadva
+                    </h4>
+                    Ha úgy gondolod, hogy ennek nem így kéne működnie írj egy emailt a <b>benedekb97@gmail.com</b> címre
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
