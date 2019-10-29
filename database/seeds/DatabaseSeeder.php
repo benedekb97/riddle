c2b9d526b0f0bcbcdd9b739ccf28b8468801f1fc
Benedek Burgess <benedekb97@gail.com>
2019-10-29 11:06:35 +0100

    update seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 //        $this->call(SettingSeeder::class);
-        $this->call(LogTypeSeeder::class);
         $this->call(LogCategorySeeder::class);
+        $this->call(LogTypeSeeder::class);
     }
 }
